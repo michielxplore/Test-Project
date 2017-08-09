8be7b8317b1e5e2ee72fdcc6ef87caba5a116c24
michielxplore <michiel.s.xplore@gmail.com>
2017-08-09 21:34:05 +0200

    Start Test Project

---
+++ TestClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * TestClass.cpp
+ *
+ *  Created on: 09 Aug 2017
+ *      Author: michielsaelen
+ */
+
+#include "TestClass.h"
+
+TestClass::TestClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TestClass::~TestClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TestClass.h
@@ -0,0 +1,17 @@
+/*
+ * TestClass.h
+ *
+ *  Created on: 09 Aug 2017
+ *      Author: michielsaelen
+ */
+
+#ifndef TESTCLASS_H_
+#define TESTCLASS_H_
+
+class TestClass {
+public:
+	TestClass();
+	virtual ~TestClass();
+};
+
+#endif /* TESTCLASS_H_ */
